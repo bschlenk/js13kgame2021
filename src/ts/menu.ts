0f1ba8c64f35dfbc591bf7dc634069a0308807b3
Dustin Toff <me@dustintoff.com>
2021-08-15 10:57:04 -0400

    Added pause menu

---
+++ src/ts/menu.ts
@@ -0,0 +1,19 @@
+import { canvas, canvasContext, clearCanvas } from './canvas';
+
+function renderPauseMenu() {
+  clearCanvas();
+  canvasContext.fillStyle = '#222';
+  canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+  drawCenterText('Plannet Hoppers', 6, 150);
+  drawCenterText('Press Space to Resume', 3, 300);
+}
+
+function drawCenterText(text: string, size: number, yOffset: number) {
+  canvasContext.strokeStyle = '#eee';
+  canvasContext.font = `${size}em sans-serif`;
+  const middleCanvas = canvas.width / 2;
+  const textWidth = canvasContext.measureText(text).width;
+  canvasContext.strokeText(text, middleCanvas - textWidth / 2, yOffset);
+}
+
+export { renderPauseMenu };
