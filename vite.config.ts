ec507a786e56032dfeac3dc696cd0fe7ea5c9610
Dustin Toff <me@dustintoff.com>
2021-08-21 12:57:02 -0400

    Optimize build output from 2.51 KB to 1.56 KB
    - Removes additional prelude code being generated

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite';
+import { viteSingleFile } from 'vite-plugin-singlefile';
+import { minifyHtml } from 'vite-plugin-html';
+
+// eslint-disable-next-line import/no-default-export
+export default defineConfig({
+  plugins: [
+    viteSingleFile(),
+    // Also minifies CSS
+    minifyHtml(),
+  ],
+  build: {
+    // Unnecessary for the modern browsers we target, reducing size of prelude
+    polyfillModulePreload: false,
+    // Speeds up build to not calculate
+    brotliSize: false,
+    terserOptions: {
+      mangle: {
+        properties: {
+          builtins: true,
+          reserved: [],
+        },
+      },
+    },
+  },
+});
