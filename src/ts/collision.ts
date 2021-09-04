d7d80ce1544392544cec9ef882bd4a04ab7b73c1
Dustin Toff <me@dustintoff.com>
2021-09-04 16:15:26 -0400

    Add support for player attachment to rotating planets

--- src/ts/collision.ts
+++ src/ts/collision.ts
@@ -11,36 +11,29 @@ export function doCirclesIntersect(
   return vecDistance(a.vector, b.vector) < maxDistance;
 }
 
-export function handleCollisions(
-  moveableObject: UniverseCircle,
-  universe: Universe,
-) {
+export function handleCollisions(circle: UniverseCircle, universe: Universe) {
   const universeObjects = universe.objects;
   universeObjects.forEach((universeObject) => {
-    if (
-      universeObject === moveableObject ||
-      universeObject instanceof UniversePlayer
-    ) {
+    if (universeObject === circle || universeObject instanceof UniversePlayer) {
       return;
     }
 
-    if (moveableObject instanceof UniversePlayer) {
+    if (circle instanceof UniversePlayer) {
       if (
         universeObject instanceof UniverseCircle &&
-        doCirclesIntersect(moveableObject, universeObject)
+        doCirclesIntersect(circle, universeObject)
       ) {
-        handlePlayerInteraction(moveableObject, universeObject, universe);
+        handlePlayerInteraction(circle, universeObject, universe);
       }
       return;
     }
 
     if (
       universeObject instanceof UniverseCircle &&
-      moveableObject instanceof UniverseCircle
+      circle instanceof UniverseCircle
     ) {
-      if (doCirclesIntersect(universeObject, moveableObject)) {
-        console.log('Collision!');
-        removeFromArray(universeObjects, moveableObject);
+      if (doCirclesIntersect(universeObject, circle)) {
+        removeFromArray(universeObjects, circle);
 
         // Check if the other object should go away too
         if (!universeObject.isFixed) {
